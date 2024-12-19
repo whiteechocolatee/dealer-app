a861538409877431bd7aab926936cec39929db5a
whiiteechocolatee <creatuseandr@icloud.com>
2024-12-19 16:18:56 +0200

    Replace ESLint configuration with .eslintrc.cjs file, removing the old eslint.config.js. Update package.json and package-lock.json to include new dependencies for ESLint, TypeScript, and additional libraries like tailwind-merge and lucide-react. This refactor enhances linting rules and improves project structure.

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', 'jsx-a11y', 'import', '@typescript-eslint', 'react-hooks'],
+  rules: {
+    'react/jsx-props-no-spreading': 'warn',
+    'no-param-reassign': 'off',
+    'react/require-default-props': 'warn',
+    '@typescript-eslint/dot-notation': 'off',
+    'no-console': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  ignorePatterns: [
+    '.next/',
+    '.eslintrc.cjs',
+    'components/ui/',
+    'prettier.config.js',
+    'test.tsx',
+    'tailwind.config.ts',
+    'postcss.config.mjs',
+    'next.config.mjs',
+  ],
+};
